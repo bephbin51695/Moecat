6b3f93e3844a93ef1acc7776317478fdb02497fe
Xavier <NatsumeXavier@gmail.com>
2019-07-07 15:13:29 +0800

    refactor: convert into Chinese

--- docleanup.php
+++ docleanup.php
@@ -4,28 +4,28 @@ require_once("include/bittorrent.php");
 dbconn();
 loggedinorreturn();
 if (get_user_class() < UC_SYSOP) {
-die('forbidden');
+    die('forbidden');
 }
 echo "<html><head><title>Do Clean-up</title></head><body>";
 echo "<p>";
-echo "clean-up in progress...please wait<br />";
+echo "正在清理...请稍候<br />";
 ob_flush();
 flush();
 if ($_GET['forceall']) {
-	$forceall = 1;
+    $forceall = 1;
 } else {
-	$forceall = 0;
-echo "you may force full clean-up by adding the parameter '<a  href='docleanup.php?forceall=1'>forceall=1</a>' to URL<br />";
+    $forceall = 0;
+    echo "可以通过将参数'<a  href='docleanup.php?forceall=1'>forceall=1</a>'添加到url来强制完全清除<br />";
 }
 echo "</p>";
 $tstart = getmicrotime();
 require_once("include/cleanup.php");
-print("<p>".docleanup($forceall, 1)."</p>");
+print("<p>" . docleanup($forceall, 1) . "</p>");
 $tend = getmicrotime();
 $totaltime = ($tend - $tstart);
-printf ("Time consumed:  %f sec<br />", $totaltime);
+printf("消耗时间:  %f sec<br />", $totaltime);
 
-echo "Done<br />";
+echo "O98K<br />";
 echo "</body></html>";
  
  
